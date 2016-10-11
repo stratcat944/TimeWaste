90e9c359bbbd2dadf49a7b13b190e1e2709e1dbd
Steve Domenico <stratcat944@gmail.com>
2016-10-11 16:52:09 -0600

    file upload and save to db working

--- server/controllers/profile-controller.js
+++ server/controllers/profile-controller.js
@@ -3,7 +3,17 @@ var fs = require('fs-extra');
 var path = require('path');
 
 module.exports.updatePhoto = function(req, res){
-	var file = req.body.file;
+	var file = req.files.file;
 	var userId = req.body.userId;
 	console.log("User " + userId + " is submitting " , file);
+	var uploadDate = new Date().toISOString;
+	var tempPath = file.path;
+	var targetPath = path.join(__dirname, '../../uploads/' + userId + uploadDate + file.name);
+	fs.rename(tempPath, targetPath, function(err){
+		if (err){
+			console.log(err);
+		} else {
+			console.log("File Moved");
+		}
+	})
 }
